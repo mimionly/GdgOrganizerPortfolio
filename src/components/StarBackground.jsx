c87f3e3e6c26f2ef5c5f893bfb4c6907dc4a3152
risha-bit <diasminora2613@gmail.com>
2025-12-18 15:16:44 +0530

    feat(ui): implement dark mode with star and meteor background

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -1,95 +1,109 @@
-import { useEffect, useState  } from "react";
-// id , size , x ,y opacity and animation duration 
-export const StarBackground = () =>{
-    const[stars, setStars] =useState([]);
-    const[meteors ,setMeteors] =useState([]);
+import { useEffect, useState } from "react";
+ //useEffect-> runs the code after the component loads
+ //useState-> store data that can change  (stars & meteors)
 
-    useEffect(() =>{
-        generateStars();
-        generateMeteors();
+export const StarBackground = () => {
+//react function component ,it will render an animated star and merteor background
+  const [stars, setStars] = useState([]); 
+  //stars → array holding star objects
+  const [meteors, setMeteors] = useState([]);
+ // meteors → array holding meteor objects
+ //setStars() and setMeteors() -> updat the arrays 
 
-        const handleResize =() => {
-            generateStars();
-        };
+  useEffect(() => {
+    //rns when the componenet loads 
+    generateStars();
+    generateMeteors();
+//creates stars and meteors when page loads 
+    const handleResize = () => {
+      generateStars();
+      //if screen changes -> regenerate stars 
+    };
 
+    window.addEventListener("resize", handleResize);
+//Calls handleResize whenever window size changes
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  ///Empty array → useEffect runs only once
 
-        window.addEventListener("resize" , handleResize);
+  const generateStars = () => {
+    //function to create random stars 
+    const numberOfStars = Math.floor/* floor removes decimals */(
+      (window.innerWidth * window.innerHeight) /1000
+      //more screen area-> more stars 
+    );
 
-        return () => window.removeEventListener("resize" , handleResize)
-    },[]);
+    const newStars = [];
+    //stores stars objects 
 
-    const generateStars = () => {
-        const numberOfStars = Math.floor(
-            (window.innerWidth * window.innerHeight) /10000 
-        );
-
-    const newStars=[];
-
-    for(let i=0; i < numberOfStars; i++){
-        newStars.push({
-            id : i,
-            size :  Math.random() * 3 + 1,
-            x:Math.random() * 100,
-            y:Math.random() * 100,
-            opacity : Math.random() * 0.5 + 0.5 ,
-            animationDuration : Math.random() * 4 + 2 ,
-        });
+    for (let i = 0; i < numberOfStars; i++) {
+      newStars.push({
+        id: i, //unique key 
+        size: Math.random() * 2 + 1, //1px to 3px
+        x: Math.random() * 100, //random position
+        y: Math.random() * 100,//random position 
+        opacity: Math.random() * 0.5 + 0.5, //0.5 to 1
+        animationDuration: Math.random() * 4 + 2, //2s to 6s
+      });
     }
 
+    setStars(newStars);
+    //saves stars tto state -> react re-renders
+  };
 
-    setStars[newStars];
-};
-const generateMeteors=()=> {
-    const numberOfStars =4;
-    const newMeteors =[];
+  const generateMeteors = () => {
+    //create shooting stars
+    const numberOfMeteors = 15;
+    //always 4 meteors 
+    const newMeteors = [];
 
+    for (let i = 0; i < numberOfMeteors; i++) {
+      newMeteors.push({
+        id: i, //width
+        size: Math.random() *1 + 1 , //later multiplies
+        x: Math.random() * 100, //sarting position
+        y: Math.random() * 20, //starting position
+        delay: Math.random() * 15, //random animation start
+        animationDuration: Math.random() * 3 + 3, //3-6
+      });
+    }
 
-for( let i=0 ; i< numberOfStars ; i++){
-    newMeteors.push({
-        id: i,
-        size : Math.random() * 2+1,
-        x: Math.random() *100,
-        y: Math.random() *20,
-        delay :Math.random() *15,
-        animationDuration :Math.random() *3 + 3,
-    });
-}
-setMeteors(newMeteors);
-};
+    setMeteors(newMeteors);
+    //updated state 
+  };
 
-return (
-    <div className="fixed inset-0 overflow-hidden pointer-events-none  z-0"> 
-    {stars.map((star) => ( 
-    <div
-        key ={star.id}
-        className="star animate-pulse-subtle"
-        style={{
-        width:  star.size +"px",
-        height: star.size +"px",
-        left : star.x + "%",
-        top: star.y +"%",
-        opacity: star.opacity,
-        animationDuration :star.animationDuration +"s",
-        }}
+  return (
+    <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
+  
+      {stars.map((star) => (
+        <div
+          key={star.id}
+          className="star animate-pulse-subtle"
+          style={{
+            width: star.size + "px",
+            height: star.size + "px",
+            left: star.x + "%",
+            top: star.y + "%",
+            opacity: star.opacity,
+            animationDuration: star.animationDuration + "s",
+          }}
         />
       ))}
 
-
-    {meteors.map((meteor) => (
-    <div 
-        key={meteor.id}
-        className="meteor animate-meteor"
-        style={{
-            width: meteor.size *50 + "px",
-            height:meteor.size * 2 +"px",
-            left: meteor.x +"%",
-            top : meteor.y +"%",
+      {meteors.map((meteor) => (
+        <div
+          key={meteor.id}
+          className="meteor animate-meteor"
+          style={{
+            width: meteor.size  + "px",
+            height: meteor.size * 4 + "px",
+            left: meteor.x + "%",
+            top: meteor.y + "%",
             animationDelay: meteor.delay,
-            animationDuration :meteor.animationDuration +"s",
-        }}
+            animationDuration: meteor.animationDuration + "s",
+          }}
         />
-    ))}
+      ))}
     </div>
-    );
+  );
 };
-    
--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,38 +1,58 @@
-import { Moon } from "lucide-react";
-import { Sun } from "lucide-react";
-import {useEffect, useState} from "react";
-import {cn} from "@/lib/utils";
+import { Moon, Sun } from "lucide-react";
+//icons from lucide
+import { useEffect, useState } from "react";
+//stores the current theme (dark/light)
+//runs the code whe the component loads
+import { cn } from "@/lib/utils";
+//utility to combine class names cleanly (common in Tailwind projects)
+
 export const ThemeToggle = () => {
-const [isDarkMode , setIsDarkMode] = useState(false);
+  const [isDarkMode , setIsDarkMode] =useState(false);
+//dark mode ON
+//default is false (light mode)
 
-    useEffect( () => {
-        const storedTheme =localStorage.getItem("theme")
-        if(storedTheme === "dark"){
-            setIsDarkMode(true)
-            document.documentElement.classList.add("dark")
-        }
-    },[])
-    const toggleTheme = () => {
-        if (isDarkMode){
-            localStorage.setItem("theme","light");
-            document.documentElement.classList.remove("dark");
-            setIsDarkMode(false);
-        }else{
-            localStorage.setItem("theme","dark");
-            document.documentElement.classList.add("dark");
-            setIsDarkMode(true);
-        }
-    };
+  useEffect(() /* runs only when the page loads */=> {
+    const storedTheme = localStorage.getItem("theme");//reads theme from the browser localStorage
+    //checks if the saved theme is "dark"
+    if (storedTheme === "dark") {
+      setIsDarkMode(true);
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      setIsDarkMode(false);
+    }
+  }, []);
+  //[] at the end means run only once when component mounts
 
-    return  (
-    <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", "focus:outline-hidden" 
-    )}> 
-        {" "}
-        {isDarkMode ? ( 
-            <Sun className="h-6 w-6 text-yellow-300"/> 
-        ): (
-            <Moon className="h-6 w-6 text-blue-900" />
-        )}
+  const toggleTheme = () => {
+    if (isDarkMode) {
+      document.documentElement.classList.remove("dark");
+      //Removes dark styles
+      localStorage.setItem("theme", "light");
+      //Saves "light" in localStorage
+      setIsDarkMode(false);
+    } else {
+      document.documentElement.classList.add("dark");
+      //Enables dark mode
+      localStorage.setItem("theme", "dark");
+      //Saves "dark" in localStorage
+      setIsDarkMode(true);
+    }
+  };
+
+  return (
+    <button
+      onClick={toggleTheme}
+      className={cn(
+        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        "focus:outlin-hidden"
+      )}
+    >
+      {isDarkMode ? (
+        <Sun className="h-6 w-6 text-yellow-300" />
+      ) : (
+        <Moon className="h-6 w-6 text-blue-900" />
+      )}
     </button>
-    );
-} ;
+  );
+};
