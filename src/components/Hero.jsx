7ba730c71ba0a1add2cd7e32bb1038f4f7806d45
risha-bit <diasminora2613@gmail.com>
2026-01-20 02:58:42 +0530

    fix(about):

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,31 +1,43 @@
-import { ArrowDown } from "lucide-react";
+
 export const Hero = () => {
     return (
         <section 
-        id ="hero" 
-        className="relative min-h-screen flex flex-col items-center justify-center px-4">
-        <div className ="container max-w-4xl mx-auto text-center z-10 ">
-       
-        <h1 className ="text-3xl md:text-6xl sm:text-xl font-bold tracking-tight py-4  leading-tightext-center">
-            <span className ="opacity-0 animate-fade-in"> Hi, I'm </span>
-            <span className =" opacity-0 animate-fade-in-delay-1 text-primary">  Ashley </span>
-            <span className ="text-gradient opacity-0 animate-fade-in-delay-1 text-gradient ml ">  Pinto </span>
-        </h1>
-        <p className ="text-lg md:text-xl text-muted-foreground max-2-2xl sm:text-xs mx-auto opacity-0 animate-fade-in-delay-3 ">I am a self-motivated person and a good team leader; I have organized various successful events and programs and I am adaptable to all challenging situations.
-        Excellent written and verbal communication skills, confidence, and proficiency in public speaking and team management, I am also a keen observer and good listener which aids in my ability to understand a situation and to prepare an appropriate response.
-        </p>
-        <div className =" opacity-0 animate-fade-in-delay-4 pt-7">
-            <a href="#projects" className="consmic-button"> Leadership in Action
-
-            </a>
+        id ="home" 
+        className="relative min-h-screen flex items-start md:items-center justify-center px-4 pt-12 md:pt-0"
+>
+        <div className ="container max-w-4xl mx-auto text-center z-0 ">
+       {/* main wrapper*/}
+       <div className="flex flex-col md:flex-row items-center gap-8">
+        {/* left image */}
+        <div className="w-full md:w-1/2 flex justify-center mt-0 md:mt-0">
+        <img 
+        src="/Projects/ashley.jpeg" 
+        alt="Ashley Pinto"
+        className="w-36 h-36 md:w-60 md:h-60  mx-auto object-center  object-cover rounded-full "/>
         </div>
-    </div>
-  
-   <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
-   <span className="text-sm sm:text-xs text-muted-foreground mb-2"> Scroll</span>
-        <ArrowDown className ="h-5 w-5 text-primary"/>
+        {/*right / Bottom text*/}
+        <div className="w-full md:w-1/2 text-center md:text-left">
+        <h1 className ="text-3xl md:text-xl sm:text-xl font-bold tracking-tight   leading-tight ">
 
-   </div>
+            <span className ="opacity-0 animate-fade-in lg:text-4xl"> Hi, I'm </span>
+            <span className =" opacity-0 animate-fade-in-delay-1 text-primary py-4 lg:text-4xl">  Ashley </span>
+            <span className ="text-gradient opacity-0 animate-fade-in-delay-1 text-gradient ml lg:text-4xl">  Pinto </span>
+        </h1>
+        
+        <p className ="text-center  md:text-left md:text-xl text-lg max-x-xl sm:text-xs mx-auto md:mx-0 opacity-0 animate-fade-in-delay-3 leading-relaxed">I am a self-motivated individual with strong leadership skills and hands-on
+        experience in organizing successful events and programs. I adapt quickly to
+        challenging situations and thrive in dynamic environments.
+        <br />
+        <span className="block mt-4">
+        With excellent written and verbal communication skills, confidence in public
+        speaking, and effective team management abilities, I am also a keen observer
+        and listener—allowing me to understand situations deeply and respond
+        thoughtfully.
+        </span>
+        </p>
+       </div> 
+      </div>
+     </div>
 </section> 
 );
 };
