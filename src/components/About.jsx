8d436c94e0e03d209523df2784d39e471a6cf9ec
risha-bit <diasminora2613@gmail.com>
2026-02-06 17:40:20 +0530

    Fix: About page Heading

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,14 @@ export const About = () => {
     <section id="about" className="py-24 px-4 relative">
       {" "}
       <div className="container mx-auto max-w-5xl">
-        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-12 text-center">
           About <span className="text-primary"> Me</span>
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
           <div className="space-y-6">
             
-            <h3 className="text-2xl md:text-2xl  mb-12 text-center font-semibold">
+            <h3 className="text-xl md:text-2xl  mb-12 text-center font-semibold">
               GDG On Campus SJEC
             <span className="text-primary"> Organizer </span>
             </h3>
