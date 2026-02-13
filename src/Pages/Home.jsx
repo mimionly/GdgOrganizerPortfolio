7c76a644a8f5eb3fa2da72ebf127a321139a01a5
risha-bit <diasminora2613@gmail.com>
2026-02-13 21:22:43 +0530

    Redeploy #1

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "@/components/StarBackground";
+import { StarBackground } from "../components/StarBackground";
 import { Nav } from "../components/Nav";
 import { Hero } from "../components/Hero";
 import { About } from "../components/About";
