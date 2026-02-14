79b9ae75e05caaa8807af3d168c8f29ec0bb0767
risha-bit <diasminora2613@gmail.com>
2026-02-14 19:15:45 +0530

    Fix : the image aligned below the cap

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -5,17 +5,18 @@ export const NotFound = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center text-center px-4 bg-gray-50 text-gray-800 font-sans">
       <div className='block'>
-                          <img
-                              src={404} 
-                              alt="44" 
-                              className='w-full h-auto object-cover opacity-50'
-                          />
-                      </div>
+          <img
+              src="/404.png" // Fixed path here!
+              alt="404 Page Not Found" 
+              className='w-72 h-auto object-cover opacity-50'
+          />
+          <p className='text-sm '> Error 404: Free Time Missing, Organizer.exe still running </p>
+      </div>
       
       {/* Call to Action Button */}
       <Link 
         to="/" 
-        className="px-6 py-3 text-base font-semibold text-white bg-blue-500 hover:bg-blue-600 rounded-lg shadow-md transition-colors duration-200 ease-in-out"
+        className="px-6 py-3 text-base font-semibold  rounded-lg transition-colors duration-200 ease-in-out mt-8"
       >
         Return to Home
       </Link>
